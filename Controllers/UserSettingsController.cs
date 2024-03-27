cf3d70b21934892255ba9103071052bffe72a626
Ebby Chakkungal <121340114+EbbyChack@users.noreply.github.com>
2024-03-27 12:54:38 +0100

    added user settings controller with update password action

--- Controllers/UserSettingsController.cs
+++ Controllers/UserSettingsController.cs
@@ -30,7 +30,21 @@ namespace RecipeWebsite.Controllers
             }
 
             // Find the user in the database
-            var user = await _context.Users.FindAsync(int.Parse(userId));
+            var user = await _context.Users.Select(u => new 
+            {
+                UserId = u.UserId,
+                Username = u.Username,
+                Email = u.Email,
+                Name = u.Name,
+                Surname = u.Surname,
+                DateOfBirth = u.DateOfBirth,
+
+                // Include the user's comments
+                Comments = u.Comments.Where(c => c.IsActive),
+                Ratings = u.Ratings
+                
+            }).FirstOrDefaultAsync(u => u.UserId == int.Parse(userId));
+
             if (user == null)
             {
                 return NotFound();
@@ -40,6 +54,38 @@ namespace RecipeWebsite.Controllers
 
         }
 
-       
+        //Update password
+        // PUT /api/usersettings/updatepassword
+        [HttpPut("updatepassword")]
+        public async Task<ActionResult<User>> UpdatePassword(UpdatePasswordDto updatePasswordDto)
+        {
+            // Get the user id from the User property
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId == null)
+            {
+                return Unauthorized();
+            }
+
+            // Find the user in the database
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserId == int.Parse(userId));
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            // Check if the old password is correct
+            if (!BCrypt.Net.BCrypt.Verify(updatePasswordDto.OldPassword, user.Password))
+            {
+                return BadRequest("Old password is incorrect");
+            }
+
+            // Update the password
+            user.Password = BCrypt.Net.BCrypt.HashPassword(updatePasswordDto.NewPassword);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(user);
+        }
     }
 }
---
+++ Models/updatePasswordDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RecipeWebsite.Models
+{
+    public partial class UpdatePasswordDto
+    {
+        public string OldPassword { get; set; } = null!;
+
+        public string NewPassword { get; set; } = null!;
+    }
+}
