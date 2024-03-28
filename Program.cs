1d0f3d1eb88e75caa7b42b92f7184e816c8d87d2
Ebby Chakkungal <121340114+EbbyChack@users.noreply.github.com>
2024-03-28 18:25:50 +0100

    fixed cors

--- Program.cs
+++ Program.cs
@@ -13,6 +13,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 //builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        "AllowSpecificOrigin",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:3000")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        }
+    );
+});
+
 //for testing authorization
 builder.Services.AddSwaggerGen(c =>
 {
@@ -78,6 +91,9 @@ if (app.Environment.IsDevelopment())
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
     });
 }
+
+app.UseCors("AllowSpecificOrigin");
+
 
 app.UseHttpsRedirection();
 
