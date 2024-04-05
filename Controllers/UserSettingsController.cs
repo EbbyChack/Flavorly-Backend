3aa9f372fbdc4286e45d3ea06699cfbc2b371217
Ebby Chakkungal <121340114+EbbyChack@users.noreply.github.com>
2024-04-05 18:24:25 +0200

    fixed userfav problem

--- Controllers/UserSettingsController.cs
+++ Controllers/UserSettingsController.cs
@@ -19,18 +19,11 @@ namespace RecipeWebsite.Controllers
         }
 
         // GET /api/usersettings
-        [HttpGet]
-        public async Task<ActionResult<User>> GetUserSettings()
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUserSettings(int id)
         {
-            // Get the user id from the User property
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            if (userId == null)
-            {
-                return Unauthorized();
-            }
-
             // Find the user in the database
-            var user = await _context.Users.Select(u => new 
+            var user = await _context.Users.Where(u => u.UserId == id).Select(u => new 
             {
                 UserId = u.UserId,
                 Username = u.Username,
@@ -43,7 +36,7 @@ namespace RecipeWebsite.Controllers
                 Comments = u.Comments.Where(c => c.IsActive),
                 Ratings = u.Ratings
                 
-            }).FirstOrDefaultAsync(u => u.UserId == int.Parse(userId));
+            }).FirstOrDefaultAsync();
 
             if (user == null)
             {
@@ -56,18 +49,13 @@ namespace RecipeWebsite.Controllers
 
         //Update password
         // PUT /api/usersettings/updatepassword
-        [HttpPut("updatepassword")]
-        public async Task<ActionResult<User>> UpdatePassword(UpdatePasswordDto updatePasswordDto)
+        [HttpPut("updatepassword/{id}")]
+        public async Task<ActionResult<User>> UpdatePassword(int id,UpdatePasswordDto updatePasswordDto)
         {
-            // Get the user id from the User property
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            if (userId == null)
-            {
-                return Unauthorized();
-            }
+           
 
             // Find the user in the database
-            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserId == int.Parse(userId));
+            var user = await _context.Users.Where(u => u.UserId == id).FirstOrDefaultAsync();
 
             if (user == null)
             {
